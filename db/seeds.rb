3b24fecbb74ed59ed487755890bc16d3e93797c7
Grace Yang <yaycake@gmail.com>
2017-11-24 16:50:37 +0800

    seeding part 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,20 +11,84 @@
 require 'open-uri'
 require 'json'
 
-url = "http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+# url = "http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
 
-response = open(url).read
+# response = open(url).read
 
-data = JSON.parse(response)
+# data = JSON.parse(response)
 
-ingredients = data["drinks"]
+# ingredients = data["drinks"]
 
-ingredients.each do |ingredient|
-  Ingredient.create(name:ingredient["strIngredient1"])
-end
+# ingredients.each do |ingredient|
+#   Ingredient.create(name:ingredient["strIngredient1"])
+# end
 
 
 # - - - - - - - - - - SEEDING COCKTAILS
+
+# seed from "http://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail"
+# (provides cocktail name as strDrink), (cocktail image URL as strDrinkThumb)
+# and provies idDrink as idDrink
+# ===> nest iteration for URL = "http://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=#{idDrink}"
+#     (provides instructions as strInstructions), (ingredients as strIngredient 1-15)
+#     (measurements (dose descrip) as strMeasure(corresponding  ingredients)
+
+
+c_url = "http://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail"
+
+c_response = open(c_url).read
+
+c_data = JSON.parse(c_response)
+
+cocktails = c_data["drinks"]
+
+cocktails.each do |cocktail|
+  sleep(5)
+  new_cocktail = Cocktail.new(name: cocktail["strDrink"])
+
+  drink_url = "http://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=#{cocktail["idDrink"]}"
+
+  drink_response = open(drink_url).read
+
+  drink_data = JSON.parse(drink_response)#json hash of array of hash
+
+  drink = drink_data["drinks"][0] #a hash of singular drink detail
+
+    # drink.each do |kvpair|
+        # p kvpair
+      new_cocktail.photo = drink["strDrinkThumb"] # set the photo url
+       i_pattern =  /^strIngredient\d?\d$/  #ingredients pattern
+       m_pattern = /^strMeasure\d?\d$/
+        p ingredients_array = drink.select {|key, value| key =~ i_pattern}.to_h.values.compact.reject{|x| x.strip==""}
+        p measurements_array = drink.select {|key, value| key =~ m_pattern}.to_h.values.compact.reject{|x| x.strip==""}
+        ingredients_array.each do |ingredient|
+          if Ingredient.where(name: ingredient)
+            next
+          else
+            new_ingredient = Ingredient.new(name: ingredient)
+            new_ingredient.save!
+            p new_ingredient.errors.full_messages
+          end
+          Dose.create()
+
+        end
+      # end
+      # create a new dose with the the same indexes of both arrays:
+
+        ###OR: create an ingredients array if value is not ""
+        #####look for ingredient id, add to new_cocktail
+        # create a new dose: look for description
+        ###OR: create new description array
+
+
+    end
+
+
+
+
+
+
+
 
 # require 'nokogiri'
 
@@ -34,8 +98,12 @@ end
 # html_doc = Nokogiri::HTML(html_file)
 
 # html_doc.search('.blog_list_item_lists').each do |element|
-#   Cocktail.create(name: element.css('.blog_text')
 
+#   new_cocktail = Cocktail.new(name: element.css('.blog_text'))
+#     html_doc.search('li').each do |ingredient|
+#       new_dose = Dose.new()
+#     end
+#   end
 
 #   puts element.text.strip
 #   puts element.attribute('href').value

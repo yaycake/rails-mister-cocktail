60278a0f95e60cb3eedfe1d68575f6ad674aba19
Grace Yang <yaycake@gmail.com>
2017-11-24 11:54:59 +0800

    Add figaro, installed cloudinary

--- Gemfile
+++ Gemfile
@@ -21,7 +21,7 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.2'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
-
+gem 'figaro'
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
@@ -37,7 +37,8 @@ gem 'font-awesome-sass'
 gem 'simple_form'
 gem 'autoprefixer-rails'
 gem 'jquery-rails'
-
+gem 'nokogiri'
+gem 'cloudinary'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# - - - - - - - - - - SEEDING INGREDIENTS
+
 require 'open-uri'
 require 'json'
 
@@ -19,3 +22,21 @@ ingredients = data["drinks"]
 ingredients.each do |ingredient|
   Ingredient.create(name:ingredient["strIngredient1"])
 end
+
+
+# - - - - - - - - - - SEEDING COCKTAILS
+
+# require 'nokogiri'
+
+# cocktails_url = "http://iba-world.com/iba-cocktails/"
+
+# html_file = open(url).read
+# html_doc = Nokogiri::HTML(html_file)
+
+# html_doc.search('.blog_list_item_lists').each do |element|
+#   Cocktail.create(name: element.css('.blog_text')
+
+
+#   puts element.text.strip
+#   puts element.attribute('href').value
+# end
